2bd63eaea3de9c13599b7fb86f6f3b877896facf
1eedaegon <d8726243@gmail.com>
2021-12-05 23:51:53 +0900

    happy nest :)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController, CatsController],
+  imports: [AuthModule, UserModule],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
